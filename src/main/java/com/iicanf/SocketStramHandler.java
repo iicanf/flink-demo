d025f11430a314dc690ca954f6d3fe3041698d3e
guozhenhua <toohardforname@gmail.com>
2018-07-26 16:00:22 +0800

    write the word count demo,code form the flink website

--- src/main/java/com/iicanf/SocketStramHandler.java
+++ src/main/java/com/iicanf/SocketStramHandler.java
@@ -1,8 +1,13 @@
 package com.iicanf;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.flink.util.Collector;
 
 public class SocketStramHandler {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -19,15 +24,24 @@ public class SocketStramHandler {
 
         StreamExecutionEnvironment env = bootstrap.createStreamExecutionEnvironment();
 
-//        DataStream<Tuple2<String, Integer>> dataStream = env
-//                .socketTextStream("localhost", 9999)
-//                .flatMap(new Splitter())
-//                .keyBy(0)
-//                .timeWindow(Time.seconds(5))
-//                .sum(1);
-//
-//        dataStream.print();
-//
-//        env.execute("Window WordCount");
+        DataStream<Tuple2<String, Integer>> dataStream = env
+                .socketTextStream("localhost", 9999)
+                .flatMap(new Splitter())
+                .keyBy(0)
+                .timeWindow(Time.seconds(5))
+                .sum(1);
+
+        dataStream.print();
+
+        env.execute("Window WordCount");
+    }
+
+    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        @Override
+        public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+            for (String word : sentence.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
     }
 }
